fcaf63245bc01f914d7463cb9802cabad6289eb7
Yaniv Kessler <yanivk@gmail.com>
2023-07-01 03:18:28 +0300

    initial

---
+++ index.mjs
@@ -0,0 +1,129 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import fs from 'node:fs/promises'
+import config from './lib/config.mjs'
+import inquirer from 'inquirer'
+import { createApi } from './lib/openai.mjs'
+import { homedir } from 'node:os'
+import path from 'node:path'
+
+async function main() {
+  const { version } = JSON.parse(await fs.readFile('./package.json'))
+  const program = new Command()
+
+  program
+    .name('kes')
+    .description('kessler AI assistant (@kessler/assist)')
+    .version(version)
+    .action(genericAIPrompt)
+
+  program
+    .command('init')
+    .description('run once to init the cli')
+    .action(init)
+
+  program
+    .command('code [prompt]').alias('c')
+    .description('send a code prompt')
+    .action(codeAIPrompt)
+
+  //program.command('config [operation]', )
+
+  program.parse()
+}
+
+main()
+
+async function init() {
+  const fullConfigPath = path.join(homedir(), '.config', 'kessler_assist')
+  try {
+    await fs.access(fullConfigPath)
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      throw e
+    }
+  }
+
+  const key = await prompt('openAI api key:')
+  if (key === '') {
+    console.log('cancelling...')
+    return
+  }
+
+  await fs.writeFile(fullConfigPath, JSON.stringify({ openai: { key } }))
+}
+
+async function genericAIPrompt(options, command) {
+  checkInitialized()
+
+  let context = []
+  
+  let content = await prompt('[chatgpt]:')
+  if (content !== '') {
+    context.push({ role: 'user', content })
+  }
+
+  const openai = createApi(config.openai)
+
+  while (content !== '') {
+    const response = await openai.chat(...context)
+
+    console.log(response.data.choices.map(choice => choice.message.content).join('\n'))
+
+    content = await prompt('[chatgpt]:')
+    if (content !== '') {
+      context.push({ role: 'user', content })
+    }
+  }
+}
+
+async function codeAIPrompt(str, options, command) {
+  checkInitialized()
+
+  let userCode = str
+
+  if (!userCode) {
+    userCode = await openPromptWithEditor('type a code related query')
+
+    if (userCode === '') {
+      console.log('nothing to do, exiting...')
+      return
+    }
+  }
+
+  const openai = createApi(config.openai)
+
+  const response = await openai.chat({
+    role: 'system',
+    content: 'when asked to write code, you will reply with code only in pure textual form'
+  }, {
+    role: 'user',
+    content: `do not include any introduction in your response. write only code: ${userCode}. do not include any introduction in your response. write only code`
+  })
+
+  console.log(response.data.choices.map(choice => choice.message.content).join('\n'))
+}
+
+async function openPromptWithEditor(message) {
+  let result = await prompt(`${message} (hit enter to open the editor):`)
+
+  if (result === '') {
+    result = await prompt(`${message}:`, 'editor')
+  }
+
+  return result
+}
+
+function checkInitialized() {
+
+  if (!config.openai.key) {
+    console.log('run init command first')
+    throw new Error('not initialized')
+  }
+}
+
+async function prompt(message, type = 'input') {
+  const { answer } = await inquirer.prompt([{ message, type, prefix: '', name: 'answer' }])
+  return answer
+}
